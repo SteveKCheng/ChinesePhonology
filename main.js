7162ef609865a0a2aabf042e2255e9f4224649f8
Steve Cheng <steve.ckp@gmail.com>
2025-05-04 11:51:51 -0400

    Refresh HTML output to source revision d16458f923497d8a2260f08d6419a68a71e7b82b

--- main.js
+++ main.js
@@ -23,11 +23,62 @@ async function playSound(url)
     await new Audio(cachedUrl).play();
 }
 
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", function(e) 
+{
     const items = document.querySelectorAll("a.audio");
     for (const item of items)
     {
         url = item.getAttribute("href");
         item.addEventListener("click", handlePlaySound.bind(null, url), false);
     }
+
+    //
+    // Set up initial state of the sidebar.
+    //
+    // For media other than interactive screens, e.g. print, 
+    // the classes set on divBody and divToc do nothing as there are no
+    // CSS rules for them; and the elements are always displayed statically
+    // near the beginning of the document.
+    // 
+
+    const divBody = document.getElementById("body");
+    const divToc = document.getElementById("toc");
+
+    function toggleSidebar(toExpand) {
+        if (toExpand)
+        {
+            divToc.className = "expanded";
+            divBody.className = "sidebar-expanded";    
+        }
+        else
+        {
+            divToc.className = "collapsed";
+            divBody.className = "sidebar-collapsed";    
+        }
+    }
+
+    const divSidebarButton = document.getElementById("sidebar-button");
+    divSidebarButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        toggleSidebar(divToc.className == "collapsed");
+    });
+
+    // Pass through clicks on the content of divSidebarButton to the above
+    // handler.  We set this CSS rule dynamically here so it does not apply
+    // when JavaScript code does not run; then the button's behavior falls back
+    // to being a plain link expressed through the <a> element in the HTML source.
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "div#sidebar-button * { pointer-events: none; }"
+    );
+
+    // Make sidebar collapsed any time the user resizes the browser window too narrow.
+    // (If no JavaScript code ever runs, the table of contents is set by CSS to be
+    // always displayed statically near the beginning of the document.)
+    const narrowScreenQuery = window.matchMedia("screen and (width <= 40em)");
+    function onNarrowScreenQueryChange() {
+        toggleSidebar(!narrowScreenQuery.matches);
+    }
+
+    onNarrowScreenQueryChange();
+    narrowScreenQuery.addEventListener("change", onNarrowScreenQueryChange);
 });
